151724059607b56e40beea2b42985d273afbc4ac
calvinjgs <calvin0_0@hotmail.com>
2018-03-03 14:22:29 -0800

    fixed indents (for now)

--- app/game_state.py
+++ app/game_state.py
@@ -4,18 +4,17 @@ class game_state():
 
 	#initialize
 	def __init__(self, data):
+     self.height = data["height"]
+     self.width = data["width"]
      self.food = np.zeros((len(data["food"], 2)), dtype=int)
      for i in range(len(data["food"])):
          self.food[i,0] = data["food"]["data"][i]["x"]
          self.food[i,0] = data["food"]["data"][i]["y"]
     self.othersnakes = []
+    self.thissnake = snake(data["you"])
     for i in range(len(data["snakes"]["data"])):
-        self.othersnakes.append(snake(data["snakes"]["data"]["body"]["data"]))
-    self.thissnake = snake(data["you"]["body"]["data"])
-    self.health = data["health"]
-    self.length = data["length"]
-
-
+        if self.thissnake.id != data["snakes"]["data"][i]["id"]:
+            self.othersnakes.append(snake(data["snakes"]["data"][i]))
 
  
 	def move():
@@ -27,6 +26,13 @@ class game_state():
 
 class snake():
     def __init__(self, data_snake):
+        self.health = data_snake["health"]
+        self.id = data_snake["id"]
+        self.length = data_snake["length"]
+        self.name = data_snake["name"]
+
+        bd = data_snake["body"]
+        
         self.body = np.zeros((len(data_snake),2), dtype=int)
         for i in range(self.body.shape[0]):
             self.body[i,0] = data_snake[i]["x"]
