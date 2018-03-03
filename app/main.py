a4547a5470be874ade9fabd93c020d063c122287
spectrumpositive <jlower1@gmail.com>
2018-03-03 11:24:31 -0800

    update my app

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import random
 
 @bottle.route('/')
 def static():
-    return "the server is running, but there is not snake?"
+    return "the server is running, but can we win?"
 
 
 @bottle.route('/static/<path:path>')
