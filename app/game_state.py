68d33823e2b7eb111214c138ad82f91228d35572
Jacob Lower <jlower1@gmail.com>
2018-03-03 12:05:05 -0800

    game state method

---
+++ app/game_state.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class game_state():
+	def __init__():
+	#initialize
+	def read_data():
+	#get data
+	def move():
+	#find best move
+	def find_food():
+	#find safest food
+	def read_board():
+	#read board state
