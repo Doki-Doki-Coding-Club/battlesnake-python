4cea976d9f6b2b947038d2c0192aeb1393c32ea3
Robert <r.craig.email@gmail.com>
2018-03-03 16:56:43 -0800

    asdf

--- app/flood_fill.py
+++ app/flood_fill.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-import game_state
+from game_state import game_state 
 
 def flood_fill(data, max_depth):
     board = data.board[:]
@@ -14,33 +14,29 @@ def flood_fill(data, max_depth):
 
 
 def find_next_move(data, board, x, y, depth):
+        #print("in fnm")
         if depth == 0:
             return 0
+        board = board[:]
 
-        board[x,y] = 3
         
         if data.thissnake.tail is not data.thissnake.body[-2]:
             board[data.thissnake.tail[0], data.thissnake.tail[1]] = 0
         
-        if x < 0 or y < 0 or x > board.shape[0] or y > board.shape[1]:
+        if x < 0 or y < 0 or x >= board.shape[0] or y >= board.shape[1]:
+            #print("move is off board:",x,y)
             return -3
         if board[x,y] >= 2 and [x, y] not in data.alltails():
+            #print("move is into snake:",x,y)
             return -3
+        
+        board[x,y] = 3
         
         right = find_next_move(data, board, x+1, y, depth-1) + 1
         left = find_next_move(data, board, x-1, y, depth-1) + 1
         up = find_next_move(data, board, x, y-1, depth-1) + 1
         down = find_next_move(data, board, x, y+1, depth-1) + 1
         
-        return ["right", "left", "up", "down"][np.argmax([right, left, up, down])]
-
-
-
-def __main__():
-    gs = game_state.game_state(game_state.get_example_raw())
-    print "flood fill unit test"
-    print flood_fill(gs, 5)
-    print ""
-
-if __name__ == "__main__":
-    __main__()
+        #print([right, left, up, down][np.argmax([right, left, up, down])])
+        return [right, left, up, down][np.argmax([right, left, up, down])]
+        
